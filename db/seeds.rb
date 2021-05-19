33fcb64aad4c9383048162be1c1e5e7e801e447c
Emlyne Charbonnier <emlyne.charbonnier@gmail.com>
2021-05-19 17:39:17 +0200

    simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+4.times do
+  Flat.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    description: Faker::Restaurant.description,
+    price_per_night: Faker::Number.decimal(l_digits: 2),
+    number_of_guests: Faker::Number.within(range: 1..10)
+    )
+end
